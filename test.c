d59b1db51693d518c7c13e13af0fabe2ec25ce7b
bingxu Shi <bingxushi@sz-dl-108.autox.sz>
2021-11-12 10:12:21 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int n,i,flag = 1;
+	printf("Enter any number:");
+	scanf("%d",&n);
+
+	for(i = 2;i < n/2; ++i)
+		if(n % i == 0){
+			flag = 0;
+			break;
+		}
+	if(flag == 1)
+		printf("\nThe given number is prime");
+	else
+		printf("\nThe given number is not prime");
+	puts("\n");
+
+	return 0;
+}
